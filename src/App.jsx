a878c021cef5f97af72892c6eaa7837f925e4c2b
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-06-04 20:38:40 +0200

    Add Header component with logo placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import mainFilm from "./assets/img/filmMain.jpg";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mainFilmImage, setmainFilmImage] = useState(mainFilm);
+  function handleMainFilmImageChange() {
+    setmainFilmImage();
+  }
 
   return (
     <>
-      Hello world
+      <div className="min-h-screen w-full bg-cover bg-center bg-no-repeat flex items-center justify-center"
+        style={{ backgroundImage: `url(${mainFilmImage})` }}></div>
+      <Header />
+      <div>Hello world</div>
     </>
   )
 }
