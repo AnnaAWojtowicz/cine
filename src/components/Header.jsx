19c438e69fac16812638409e7e4f777408ad9c4e
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-06-04 20:45:44 +0200

    feat: update Header component to display logo and remove max-width from App.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from '../assets/img/cineLogo.svg';
+
 export default function Header() {
     return (
         <>
-            logo
+            <img src={logo} alt="ciné logo" className="w-16 h-16 absolute top-4 left-4" />
         </>
     )
 }
