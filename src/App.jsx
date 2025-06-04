2e06e2f95d1eb8edc93782b402bc3f12722ef30c
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-06-04 21:03:46 +0200

    feat: add FilmDetails component and update App to include it; enhance index.html with font preconnects

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header'
 import mainFilm from "./assets/img/filmMain.jpg";
+import Header from './components/Header'
+import FilmDetails from './components/FilmDetails'
+
 
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
       <div className="min-h-screen w-full bg-cover bg-center bg-no-repeat flex items-center justify-center"
         style={{ backgroundImage: `url(${mainFilmImage})` }}></div>
       <Header />
+      <FilmDetails />
       <div>Hello world</div>
     </>
   )
---
+++ src/components/FilmDetails.jsx
@@ -0,0 +1,19 @@
+export default function filmDetails() {
+    return (
+        <div>
+            <div>Title</div>
+            <div>Director</div>
+            <div>
+                <span>Country</span>
+                <span>Year</span>
+            </div>
+            <div>Time</div>
+            <div>
+                <span>play</span>
+                <span>add to fav symbol/toggle</span>
+                <span>more</span>
+            </div>
+
+        </div>
+    )
+}
