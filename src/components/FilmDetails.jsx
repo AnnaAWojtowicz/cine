dabc66429f50a3f82543374f75a2e389fc7930f6
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-06-05 12:05:05 +0200

    feat: enhance FilmDetails component layout and improve text truncation; add Material Symbols stylesheet link

--- src/components/FilmDetails.jsx
+++ src/components/FilmDetails.jsx
@@ -1,18 +1,17 @@
 export default function FilmDetails() {
     return (
-        <div className="fixed top-10 right-10 w-1/3 border-1 border-solid rounded-l-full z-50 text-[var(--white)] border-white">
-            <div className="p-4 pl-24 text-right">
-                <div className="text-xl font-bold">Title</div>
+        <div className="fixed w-1/3 top-10 right-10 border-1 border-solid rounded-l-full z-50 text-[var(--white)] border-[var(--white)]">
+            <div className="p-4 pl-24 w-full text-right">
+                <div className="text-xl font-bold truncate">TitleTitleTitleTitleTitleTitleTitle</div>
                 <div className="mt-2">Director</div>
-                <div className="mt-2 flex justify-between">
-                    <span>Country</span>
-                    <span>Year</span>
+                <div className="mt-2 ">
+                    Country Year
                 </div>
                 <div className="mt-2">Time</div>
                 <div className="mt-4 flex justify-between">
-                    <span>play</span>
-                    <span>add to fav symbol/toggle</span>
-                    <span>more</span>
+                    <span className="material-symbols-outlined cursor-pointer border-1 border-solid rounded-full border-[var(--white)]">play_arrow</span>
+                    <span className="material-symbols-outlined cursor-pointer border-1 border-solid rounded-full border-[var(--white)]">add</span>
+                    <span className="material-symbols-outlined cursor-pointer border-1 border-solid rounded-full border-[var(--white)]">more_horiz</span>
                 </div>
             </div>
         </div>
