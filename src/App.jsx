f472ad18a892293ea0596bd05eb32a7809c9e429
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-06-11 15:39:47 +0200

    feat: enhance Header component with search input and user image; update layout styling

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,12 @@ function App() {
 
   return (
     <>
+      <Header />
       <div className="min-h-screen w-full bg-cover bg-center bg-no-repeat flex items-center justify-center mb-16"
         style={{ backgroundImage: `url(${mainFilmImage})` }}>
         <FilmDetailsMain />
       </div>
-      <Header />
+
       <div className="w-full max-w-7xl mx-auto px-2">
         <FilmSection sectionName={"New"} />
         <FilmSection sectionName={"Your collection"} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import logo from '../assets/img/cineLogo.svg';
+import filmMain from '../assets/img/filmMain.jpg';
 
 export default function Header() {
     return (
-        <>
-            <img src={logo} alt="ciné logo" className="w-16 h-16 absolute top-4 left-4" />
-        </>
+        <div className="bg-[var(--white)] text-[var(--black)] w-full h-20 px-6">
+            <div className="flex justify-between">
+                <span className="flex-none">
+                    <img src={logo} alt="ciné logo" className="w-16 h-16" /></span>
+                <span className="flex-initial">
+                    <input name="search" type="text" placeholder="Search" className="w-96 h-10 px-4 border-1 border-solid border-[var(--black)] rounded-full focus:outline-none focus:border-[var(--primary)]" />
+                </span>
+                <span className="flex-none"><img src={filmMain} alt="user image" className="w-16 h-16 border-1 border-solid rounded-full" /></span>
+            </div>
+        </div>
     )
 }
