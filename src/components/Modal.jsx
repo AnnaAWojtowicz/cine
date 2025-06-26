3ca1f3ee46fc35e500add6465546f82b64629892
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-06-26 12:13:11 +0200

    style: enhance Modal component styling and adjust Profile layout margin

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ export default function Modal({ ref, children }) {
 
 
     return (
-        <dialog ref={ref} className="">
+        <dialog ref={ref} className="modal fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 shadow-lg border-1 border-solid border-[var(--orange-web)]">
             {children} this is my modal
             <form method="dialog"><button>Close</button></form>
         </dialog>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,7 @@ export default function Profile() {
     let year = d.getFullYear();
 
     return (
-        <div className="flex justify-center items-center min-h-screen ">
+        <div className="flex justify-center items-center min-h-screen mt-8">
             <div className="flex flex-col justify-start w-1/3 min-w-[339px] min-h-[100px] bg-[var(--white-600)] text-[var(--black)] border-1 border-solid rounded-t-full border-[var(--orange-web)] space-y-8 p-8">
                 <div className="flex flex-col justify-center items-center">
                     <img src={profile} alt="user image" className="w-40 h-40 border-1 border-solid border-[var(--orange-web)] rounded-full" />
