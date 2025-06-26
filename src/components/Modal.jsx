faac2eb9f1389385839165035b41aa740c4bc381
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-06-26 14:17:16 +0200

    refactor: restructure Modal component for improved layout and readability

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,13 +6,23 @@ export default function Modal({ isOpen, caption, info, onClose }) {
     if (!isOpen) return null;
 
     return (
-        <div className="modal fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 shadow-lg border-1 border-solid border-[var(--orange-web)] z-50">
-            <h2 className="text-xl font-semibold mb-4">Change your {caption}</h2>
-            <p>Your old {caption} {info}</p>
-            <div className="flex justify-between items-center mt-4 gap-4">
-                <Button buttonInput2="Confirm" />
-                <Button buttonInput1="Dismiss" onClick={onClose} />
+        <>
+            <div className="fixed inset-0 z-50 flex items-center justify-center">
+                <div
+                    className="absolute inset-0 bg-black/30"
+                    onClick={onClose}
+                />
+                <div className="modal fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 shadow-lg border-1 border-solid border-[var(--orange-web)] z-50">
+                    <h2 className="text-xl font-semibold mb-4">Change your {caption}</h2>
+                    <p>Your old {caption} {info}</p>
+                    <label>Your new {caption}</label>
+                    <input className="border-1 border-solid border-[var(--platinum-100)]"></input>
+                    <div className="flex justify-between items-center mt-4 gap-4">
+                        <Button buttonInput2="Confirm" />
+                        <Button buttonInput1="Dismiss" onClick={onClose} />
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
