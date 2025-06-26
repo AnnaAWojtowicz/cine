e271595bfa87ed0086ef6c0689417d893a882f5a
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-06-26 12:36:50 +0200

    feat: update Button component to dynamically display button text; integrate Button into Modal for improved user interaction

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,18 @@
-export default function Button({ buttonGeneralClassName }) {
+export default function Button({ buttonGeneralClassName, buttonInput1, buttonInput2 }) {
+
+    let buttonInput;
+    if (buttonInput) {
+        buttonInput = "Change";
+    } else if (buttonInput1) {
+        buttonInput = buttonInput1;
+    } else if (buttonInput2) {
+        buttonInput = buttonInput2;
+    };
+
+
     return (
         <button id="button_general" className={`bg-[var(--white)] text-[var(--black-500)] px-4 py-2 rounded transition duration-300 ${buttonGeneralClassName}`}>
-            Change
+            {buttonInput}
         </button>
     )
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button.jsx"
+
 
 export default function Modal({ ref, children }) {
 
@@ -5,7 +7,10 @@ export default function Modal({ ref, children }) {
     return (
         <dialog ref={ref} className="modal fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 shadow-lg border-1 border-solid border-[var(--orange-web)]">
             {children} this is my modal
-            <form method="dialog"><button>Close</button></form>
+            <div className="flex justify-between items-center mt-4 gap-4">
+                <form method="dialog"><Button buttonInput2="Confirm" /></form>
+                <form method="dialog"><Button buttonInput1="Dismiss" /></form>
+            </div>
         </dialog>
     )
 }
